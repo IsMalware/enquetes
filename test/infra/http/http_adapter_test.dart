996a88729dee14c2e5c0ff08cf713cd741f4a7ae
IsMalware <ismaellAquino@hotmail.com>
2021-02-23 14:28:46 -0300

    garantindo que o HttpAdapter chame o post com o header correto

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -11,9 +13,13 @@ class HttpAdapter {
   const HttpAdapter(this.client);
 
   Future<void> request({String url, HttpMethod method, Map body}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
     switch (method) {
       case HttpMethod.post:
-        client.post(url);
+        client.post(url, headers: headers);
         break;
       default:
     }
@@ -24,19 +30,29 @@ class ClientSpy extends Mock implements Client {}
 
 void main() {
   group('post', () {
-    setUp(() {});
+    HttpAdapter sut;
+    ClientSpy client;
+    String url;
+
+    setUp(() {
+      client = ClientSpy();
+      sut = HttpAdapter(client);
+      url = faker.internet.httpUrl();
+    });
 
     test('Deve chamar post com os valores corretos', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
-
       await sut.request(
         url: url,
         method: HttpMethod.post
       );
 
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ));
     });
   });
 }
